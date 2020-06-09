5dea665ee18a3325b07a8854b19aba2c8bf5357d
Edvin Boul <kittenparry@users.noreply.github.com>
2020-06-09 16:48:54 +0300

    Random character movement

--- Assets/Scripts/Game/Human.cs
+++ Assets/Scripts/Game/Human.cs
@@ -4,6 +4,10 @@ using UnityEngine.UI;
 public class Human : MonoBehaviour
 {
     Text Stats;
+    public float speed = 100;
+    private Vector2 movement;
+    public Rigidbody2D rb;
+
     public string Name = "John";
     public string LastName = "Doe";
     public string Gender = "Male";
@@ -11,23 +15,37 @@ public class Human : MonoBehaviour
 
     public int NeedHunger = 66;
     public int NeedSleep = 90;
+    public string Doing = "Wandering around...";
 
     void Start()
     {
         Stats = GameObject.Find("HumanStats").GetComponent<Text>();
         InvokeRepeating("DecreaseNeeds", 5.0f, 2.5f);
+        InvokeRepeating("CheckNeeds", 0f, 2.5f);
     }
 
     void FixedUpdate()
     {
-        string formattedText = System.String.Format("Hunger: {0}\nSleep: {1}", NeedHunger, NeedSleep);
+        string formattedText = System.String.Format("Hunger: {0}\nSleep: {1}\n{2}", NeedHunger, NeedSleep, Doing);
         Stats.text = formattedText;
+        rb.AddForce(movement * speed);
+
     }
 
+    // Human custom classes
     void DecreaseNeeds()
     {
-        NeedHunger -= 1;
-        NeedSleep -= 1;
+        NeedHunger -= 3;
+        NeedSleep -= 2;
+    }
+
+    void CheckNeeds()
+    {
+        // try to find food and stuff if too low
+        // else random movement
+        movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        movement = movement.normalized * speed * Time.deltaTime;
+        Doing = "Wandering around...";
     }
 
 }
