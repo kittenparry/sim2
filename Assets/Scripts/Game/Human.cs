7a3756a9dc8fe85a1a341c525f625bb441ce2f70
Edvin Boul <kittenparry@users.noreply.github.com>
2020-06-09 17:44:13 +0300

    Veggies in world
    Movement to veggies not working correctly at the moment
    Function need to find the difference between two Vector2s, I think

--- Assets/Scripts/Game/Human.cs
+++ Assets/Scripts/Game/Human.cs
@@ -7,6 +7,8 @@ public class Human : MonoBehaviour
     public float speed = 100;
     private Vector2 movement;
     public Rigidbody2D rb;
+    GameObject[] fruits = null;
+    Transform[] fruitsPos = new Transform[4];
 
     public string Name = "John";
     public string LastName = "Doe";
@@ -20,8 +22,14 @@ public class Human : MonoBehaviour
     void Start()
     {
         Stats = GameObject.Find("HumanStats").GetComponent<Text>();
-        InvokeRepeating("DecreaseNeeds", 5.0f, 2.5f);
+        InvokeRepeating("DecreaseNeeds", 2.5f, 2.5f);
         InvokeRepeating("CheckNeeds", 0f, 2.5f);
+
+        fruits = GameObject.FindGameObjectsWithTag("Veggies");
+        for (int i = 0; i < fruitsPos.Length; i++)
+        {
+            fruitsPos[i] = fruits[i].transform;
+        }
     }
 
     void FixedUpdate()
@@ -43,9 +51,37 @@ public class Human : MonoBehaviour
     {
         // try to find food and stuff if too low
         // else random movement
-        movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-        movement = movement.normalized * speed * Time.deltaTime;
-        Doing = "Wandering around...";
+        if (NeedHunger <= 50)
+        {
+            Transform closest = GetClosestObject(fruitsPos);
+            // transform.position = Vector3.MoveTowards(transform.position, closest.position, speed * Time.deltaTime);
+            transform.position = Vector2.Lerp(transform.position, closest.position, speed * Time.deltaTime);
+            Doing = "Looking for food.";
+        } else
+        {
+            movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            movement = movement.normalized * speed * Time.deltaTime;
+            Doing = "Wandering around...";
+        }
     }
 
+    Transform GetClosestObject(Transform[] obj)
+    {
+        // TODO: these need to be vector2
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+        foreach (Transform potentialTarget in obj)
+        {
+            Vector3 directionToTarget = potentialTarget.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget;
+            }
+        }
+
+        return bestTarget;
+    }
 }
