d1aee7576b6b20988f700522ed4da05a6b9593bf
Edvin Boul <kittenparry@users.noreply.github.com>
2020-06-09 23:48:41 +0300

    Sleep and passing out mechanics
    If can't reach bed in time
    Think if/else statements are in good order after a ton of testing
    So no more sleep walking after passing out to eat and stuff
    A house with some walls with a bed
    He tries to go through water/walls trying to reach bed,
    need to be able to go around if hits something I suppose

--- Assets/Scripts/Game/Human.cs
+++ Assets/Scripts/Game/Human.cs
@@ -7,10 +7,13 @@ public class Human : MonoBehaviour
     public float speed = 100;
     private Vector2 movement;
     public Rigidbody2D rb;
+
     GameObject[] fruits = null;
     Transform[] fruitsPos = new Transform[4];
+    GameObject[] beds = null;
+    Transform[] bedsPos = new Transform[1];
 
-    public string name = "John";
+    public string firstName = "John";
     public string lastName = "Doe";
     public string gender = "Male";
     public int age = 23;
@@ -19,19 +22,26 @@ public class Human : MonoBehaviour
     public int needSleep;
     public string charStatus = "Wandering around...";
     public bool isEating = false;
+    public bool isSleeping = false;
 
     void Start()
     {
         needHunger = Random.Range(54, 98);
-        needSleep = Random.Range(77, 98);
+        needSleep = Random.Range(44, 77);
         textStatus = GameObject.Find("HumanStats").GetComponent<Text>();
         InvokeRepeating("DecreaseNeeds", 2.5f, 2.5f);
         InvokeRepeating("CheckNeeds", 0f, 2.5f);
 
+        // a very inefficient method of doing these
         fruits = GameObject.FindGameObjectsWithTag("Veggies");
         for (int i = 0; i < fruitsPos.Length; i++)
         {
             fruitsPos[i] = fruits[i].transform;
+        }
+        beds = GameObject.FindGameObjectsWithTag("RestSpots");
+        for (int i = 0; i < beds.Length; i++)
+        {
+            bedsPos[i] = beds[i].transform;
         }
     }
 
@@ -39,16 +49,28 @@ public class Human : MonoBehaviour
     {
         string formattedText = System.String.Format("Hunger: {0}\nSleep: {1}\n{2}", needHunger, needSleep, charStatus);
         textStatus.text = formattedText;
-        if (needHunger > 50 && !isEating)
+        if (needHunger > 50 && needSleep > 30 && !isSleeping && !isEating)
         {
             rb.AddForce(movement * speed);
-        } else
+        } else if (isEating || isSleeping) { }
+        else if (needSleep <= 12 && !isSleeping)
+        {
+            charStatus = "Passed out!";
+        } else if (needHunger <= 50 && !isEating)
         {
             Transform closest = GetClosestObject(fruitsPos);
             rb.transform.position = Vector2.MoveTowards(rb.transform.position, closest.position, 3.0f * Time.deltaTime);
             if (closest.position == rb.transform.position)
             {
                 isEating = true;
+            }
+        } else if (needSleep <= 30 && !isSleeping) 
+        {
+            Transform closest = GetClosestObject(bedsPos);
+            rb.transform.position = Vector2.MoveTowards(rb.transform.position, closest.position, 3.0f * Time.deltaTime);
+            if (closest.position == rb.transform.position)
+            {
+                isSleeping = true;
             }
         }
 
@@ -58,22 +80,37 @@ public class Human : MonoBehaviour
     void DecreaseNeeds()
     {
         if (!isEating) needHunger -= 3;
-        needSleep -= 2;
+        if (!isSleeping) needSleep -= 2;
     }
 
     void CheckNeeds()
     {
         // try to find food and stuff if too low
         // else random movement
-        if (needHunger <= 50 && !isEating)
+        if (isSleeping)
         {
+            isEating = false;
+            charStatus = "Sleeping.";
+            Sleep();
+        } else if (needSleep <= 10 && !isSleeping)
+        {
+            isEating = false;
+            charStatus = "Passed out!";
+            isSleeping = true;
+            Sleep();
+        } else if (needHunger <= 50 && !isEating)
+        {
+            isSleeping = false;
             charStatus = "Looking for food...";
         } else if (isEating)
         {
+            isSleeping = false;
             charStatus = "Eating some vegetables.";
             EatFood();
-        }
-        else
+        } else if (needSleep <= 30 && !isSleeping)
+        {
+            charStatus = "Going to sleep...";
+        } else
         {
             movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
             movement = movement.normalized * speed * Time.deltaTime;
@@ -89,6 +126,16 @@ public class Human : MonoBehaviour
             isEating = false;
             charStatus = "Wandering around...";
         };
+    }
+
+    void Sleep()
+    {
+        if (needSleep < 100) needSleep = needSleep + 15 <= 100 ? needSleep + 15 : 100;
+        else
+        {
+            isSleeping = false;
+            charStatus = "Wandering around...";
+        }
     }
 
     Transform GetClosestObject(Transform[] obj)
